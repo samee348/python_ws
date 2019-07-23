a0a9a7065b528816f7e5a1bb39435b586bbd50f5
unknown <sameekshasammi1998@gmail.com>
2019-07-23 12:50:18 +0530

    updated

--- list4.py
+++ list4.py
@@ -1,3 +1,9 @@
-data="sammi,patthu,chethu,lux,manoj,mahesh,sama"
+data="sammi,patthu,chethu,lux,manoj,mahesh,sama,Arjun"
 names=data.upper().split(",")
 print(names)
+lst=[]
+for name in names:
+    if name.startswith("A") or name.endswith("H"):
+        lst.append(name)
+lst.sort()
+print()
