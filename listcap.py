a3a2d66844b2b9c712ca31150855bf02a43f1364
unknown <sameekshasammi1998@gmail.com>
2019-07-23 15:41:01 +0530

    updated

---
+++ listcap.py
@@ -0,0 +1,2 @@
+data = "raj,shiva,mani,sonu,anu,tanvi"
+print(list(map(lambda x:x.capitalize(),data.split(","))))
---
+++ listname.py
@@ -0,0 +1,2 @@
+names=["PKM","ALN","GLN","NVR","PVR","KM","VP","CS","MCS"]
+print(list(map(lambda x:len(x),names)))
